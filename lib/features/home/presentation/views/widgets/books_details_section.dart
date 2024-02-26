1552bbdf33d47cf644b977a84559b0538db26028
elesawy <elesawy928@gmail.com>
2024-02-26 23:01:29 +0200

    refactor book details body

---
+++ lib/features/home/presentation/views/widgets/books_details_section.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../../core/utils/spacing.dart';
+import '../../../../../core/utils/styles.dart';
+import 'book_rating.dart';
+import 'books_action.dart';
+import 'custom_book_image.dart';
+
+class BooksDetailsSection extends StatelessWidget {
+  const BooksDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        sizedBoxSpace(height: 24),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 85.w),
+          child: const CustomBookImage(),
+        ),
+        sizedBoxSpace(height: 30),
+        Text(
+          'The Jungle Book',
+          style: Styles.textStyle30,
+        ),
+        sizedBoxSpace(height: 6),
+        Text(
+          'Rudyard Kipling',
+          style: Styles.textStyle18.copyWith(
+            color: Colors.white.withOpacity(0.7),
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        sizedBoxSpace(height: 12),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        sizedBoxSpace(height: 30),
+        const BooksAction(),
+        Expanded(
+          child: sizedBoxSpace(height: 30),
+        ),
+      ],
+    );
+  }
+}
