ee61b542ec47517d3f9bdc8852de9f24a4f9bc2f
elesawy <elesawy928@gmail.com>
2024-02-29 08:00:09 +0200

    setup service locator

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'core/utils/service_locator.dart';
+
 void main(List<String> args) {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
